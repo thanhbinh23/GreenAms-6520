8b128fd30f45f6d11a852a6172e9eeca23663f6d
itamaroryan <itamar.oryan@gmail.com>
2022-07-07 14:33:43 +0300

    Create code from boiler plate

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/Vision.java
@@ -0,0 +1,113 @@
+package org.firstinspires.ftc.teamcode.opModes;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
+
+import java.util.List;
+
+@TeleOp(name = "Vision")
+public class Vision extends LinearOpMode {
+    private static final String TFOD_MODEL_ASSET = "FreightFrenzy_BCDM.tflite";
+    private static final String[] LABELS = {
+            "Ball",
+            "Cube",
+            "Duck",
+            "Marker"
+    };
+
+    private static final String VUFORIA_KEY =
+            "ATYVkQj/////AAABmdx7VJ/bR0zgu9d63RBYZjF51k033oLNBEUqrynEv50aSWP+EJlSZXYkFudFPS2/VPlqPUBP1IcykJVbTEEUbE0wGtDsiz1n/wdOSCgTqTsVcyKLOvEnTIpFF8fyOswXjyb7WHl4+Tioqmm6ci0RYLeSysZKn1oHpKzc6FlBGNx5EbJsZNBd37LBioDoQl0CYCHY1ezU4EJTOWrEEQGQoWopp7BsfE6wGi6L+mOPg2/pQsUidg1xg3epLWisbzSwVxtZyETLoZs4kS3kNv039PJP9ar/ez0F2ob1pcL6oENCvJFRIiyGww3pnKT1qvTlO9Ge4Bt0FNurTUvvuXi9ulOxmhEPR3+baMXspAqd9F+W";
+    private VuforiaLocalizer vuforia;
+    private TFObjectDetector tfod;
+
+    @Override
+    public void runOpMode() {
+        initVuforia();
+        initTfod();
+
+        if (tfod != null) {
+            tfod.activate();
+
+            tfod.setZoom(2.5, 16.0/9.0);
+        }
+
+        /** Wait for the game to begin */
+        telemetry.addData(">", "Press Play to start op mode");
+        telemetry.update();
+        waitForStart();
+
+        if (opModeIsActive()) {
+            while (opModeIsActive()) {
+                if (tfod != null) {
+                    // getUpdatedRecognitions() will return null if no new information is available since
+                    // the last time that call was made.
+                    List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
+                    if (updatedRecognitions != null) {
+                        telemetry.addData("# Object Detected", updatedRecognitions.size());
+
+                        // step through the list of recognitions and display boundary info.
+                        int i = 0;
+                        boolean isDuckDetected = false;     //  ** ADDED **
+                        for (Recognition recognition : updatedRecognitions) {
+                            telemetry.addData(String.format("label (%d)", i), recognition.getLabel());
+                            telemetry.addData(String.format("  left,top (%d)", i), "%.03f , %.03f",
+                                    recognition.getLeft(), recognition.getTop());
+                            telemetry.addData(String.format("  right,bottom (%d)", i), "%.03f , %.03f",
+                                    recognition.getRight(), recognition.getBottom());
+                            i++;
+
+                            // check label to see if the camera now sees a Duck
+                            if (recognition.getLabel().equals("Duck")) {
+                                isDuckDetected = true;
+                                telemetry.addData("Object Detected", "Duck");
+                                telemetry.addData("Right: ", "%.03f", recognition.getRight());
+                            } else {
+                                isDuckDetected = false;
+                            }
+                        }
+                        telemetry.update();
+
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Initialize the Vuforia localization engine.
+     */
+    private void initVuforia() {
+        /*
+         * Configure Vuforia by creating a Parameter object, and passing it to the Vuforia engine.
+         */
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
+
+        parameters.vuforiaLicenseKey = VUFORIA_KEY;
+        parameters.cameraName = hardwareMap.get(WebcamName.class, "Webcam 1");
+
+        //  Instantiate the Vuforia engine
+        vuforia = ClassFactory.getInstance().createVuforia(parameters);
+
+        // Loading trackables is not necessary for the TensorFlow Object Detection engine.
+    }
+
+    /**
+     * Initialize the TensorFlow Object Detection engine.
+     */
+    private void initTfod() {
+        int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
+                "tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
+        tfodParameters.minResultConfidence = 0.8f;
+        tfodParameters.isModelTensorFlow2 = true;
+        tfodParameters.inputSize = 320;
+        tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
+        tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABELS);
+    }
+}
