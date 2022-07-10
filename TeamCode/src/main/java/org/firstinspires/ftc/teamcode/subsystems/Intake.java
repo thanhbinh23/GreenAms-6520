e10176acdd1c33d70f6e94a6c1f35103a4058961
tranthanhbinh1412@gmail.com <75380705+thanhbinh23@users.noreply.github.com>
2022-07-10 09:20:51 +0300

    add Intake, constants and a little bit of duck

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
@@ -1,4 +1,25 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+		import com.qualcomm.robotcore.hardware.DcMotor;
+		import com.qualcomm.robotcore.hardware.HardwareMap;
+		import org.firstinspires.ftc.teamcode.Constants;
+
+
 public class Intake {
+	public DcMotor intakeMotor;
+	public HardwareMap hardwareMap;
+	public Intake (OpMode opmode){
+		hardwareMap = opmode.hardwareMap;
+	}
+	public void initIntakeMotor(){
+		intakeMotor = hardwareMap.dcMotor.get("IntakeMotor");
+	}
+
+	public void drive(){
+		intakeMotor.setPower(Constants.MOTOR.SPEEDINTAKE);
+	}
+
 }
