3e8bdf6ccf0ca3e27652cf0211c994f184c8e305
tranthanhbinh1412@gmail.com <75380705+thanhbinh23@users.noreply.github.com>
2022-07-10 17:18:24 +0300

    add Constants, arm and spinner

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -1,19 +1,39 @@
 package org.firstinspires.ftc.teamcode;
 
 public class Constants {
-    public static final class MOTOR {
-        public static final String RIGHTMASTER = "rm";
-        public static final String RIGHTFOLLOW = "rf";
-        public static final String LEFTMASTER = "lm";
-        public static final String LEFTFOLLOW = "lf";
 
-        public static final double d = 0.33;
-    }
     public static final class AUTONOMOUS{
         public static final double wheelDiameter = 0.09;
         public static final double ticksPerMeter = 336/(Math.PI* wheelDiameter);
 
     }
 
-//    public static final double angularVelocity = 2;
-}
+    public static final class MOTOR {
+            public static final String RIGHTMASTER = "rm";
+            public static final String RIGHTFOLLOW = "rf";
+            public static final String LEFTMASTER = "lm";
+            public static final String LEFTFOLLOW = "lf";
+            public static final double d = 0.33;
+        }
+        public static final class ARM {
+            public static final String TURRET_MOTOR_ID = "Turret";
+            public static final String ROTATE_MOTOR_ID = "Rotate";
+            public static final String CLAW_MOTOR_ID = "Claw";
+
+            public static final double TURRET_MOVE_LEFT = -0.5;
+            public static final double TURRET_MOVE_RIGHT = 0.5;
+
+            public static final double ARM_MOVE_UP = -1;
+            public static final double ARM_MOVE_DOWN = 0;
+
+            public static final double CLAW_OPEN = 0.5;
+            public static final double CLAW_CLOSE = -0.5;
+
+            public static final double TICKS_TO_DEGREE = 864.0/364;
+        }
+
+        public static final class SPINNER {
+            public static final String SPINNER_ID = "Spinner";
+        }
+    }
+
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Arm.java
@@ -1,5 +1,31 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
-public class Arm {
+import static org.firstinspires.ftc.teamcode.Constants.ARM;
 
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class Arm {
+    private HardwareMap hardwareMap;
+    private Telemetry telemetry;
+    private DcMotor turretMotor, armMotor, clawMotor;
+
+    public Arm(OpMode opMode) {
+        this.telemetry = opMode.telemetry;
+        this.hardwareMap = opMode.hardwareMap;
+    }
+
+    public void initArm() {
+        turretMotor = hardwareMap.dcMotor.get(ARM.TURRET_MOTOR_ID);
+        armMotor = hardwareMap.dcMotor.get(ARM.ROTATE_MOTOR_ID);
+        clawMotor = hardwareMap.dcMotor.get(ARM.CLAW_MOTOR_ID);
+
+        turretMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        armMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        clawMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+    }
 }
