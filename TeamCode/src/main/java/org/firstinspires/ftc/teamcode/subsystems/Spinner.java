1809e81d31071968661e67b7885e01224da3818a
tranthanhbinh1412@gmail.com <75380705+thanhbinh23@users.noreply.github.com>
2022-07-10 09:15:46 +0300

    Update duckspinning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Spinner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Spinner.java
@@ -2,6 +2,24 @@ package org.firstinspires.ftc.teamcode.subsystems;
 
 
 
-public class Spinner {
 
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+        import com.qualcomm.robotcore.hardware.DcMotor;
+        import com.qualcomm.robotcore.hardware.HardwareMap;
+        import org.firstinspires.ftc.teamcode.Constants;
+
+public class Spinner {
+    public DcMotor spinnerMotor;
+    public HardwareMap hardwareMap;
+    public Spinner(OpMode opmode) {
+        hardwareMap = opmode.hardwareMap;
+    }
+
+    public void initSpinner() {
+        spinnerMotor = hardwareMap.dcMotor.get("Spinner");
+    }
+
+    public void drive() {
+        spinnerMotor.setPower(Constants.MOTOR.SPEEDSPINNER);
+    }
 }
