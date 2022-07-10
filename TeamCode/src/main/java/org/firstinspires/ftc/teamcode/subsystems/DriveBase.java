f858f506c0a5213cdd555f9b7028418f1fae810f
tranthanhbinh1412@gmail.com <75380705+thanhbinh23@users.noreply.github.com>
2022-07-10 17:26:06 +0300

    delete intake and update DriveBase

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DriveBase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DriveBase.java
@@ -1,109 +1,118 @@
+
+
+
 package org.firstinspires.ftc.teamcode.subsystems;
 
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.HardwareMap;
+        import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+        import com.qualcomm.robotcore.hardware.DcMotorEx;
+        import com.qualcomm.robotcore.hardware.DcMotor;
+        import com.qualcomm.robotcore.hardware.DcMotorSimple;
+        import com.qualcomm.robotcore.hardware.HardwareMap;
 
-import static org.firstinspires.ftc.teamcode.Constants.*;
-import static org.firstinspires.ftc.teamcode.Constants.AUTONOMOUS.*;
+        import static org.firstinspires.ftc.teamcode.Constants.*;
+        import static org.firstinspires.ftc.teamcode.Constants.AUTONOMOUS.*;
 
-import org.firstinspires.ftc.robotcore.external.Telemetry;
+        import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class DriveBase {
-   public DcMotor rightMaster, rightFollow, leftMaster, leftFollow;
+    private DcMotor rightMaster, rightFollow, leftMaster, leftFollow;
 
-   private Telemetry telemetry;
-   private HardwareMap hardwareMap;
+    private Telemetry telemetry;
+    private HardwareMap hardwareMap;
 
-   public DriveBase(OpMode opMode) {
-       this.telemetry = opMode.telemetry;
-       this.hardwareMap = opMode.hardwareMap;
-   }
+    public DriveBase(OpMode opMode) {
+        this.telemetry = opMode.telemetry;
+        this.hardwareMap = opMode.hardwareMap;
+    }
 
-   public void init() {
+    public void init() {
 
-       rightMaster = hardwareMap.dcMotor.get("rm");
-       rightFollow = hardwareMap.dcMotor.get("rf");
-       leftMaster = hardwareMap.dcMotor.get( "lm");
-       leftFollow = hardwareMap.dcMotor.get( "lf");
+        rightMaster = hardwareMap.dcMotor.get("rm");
+        rightFollow = hardwareMap.dcMotor.get("rf");
+        leftMaster = hardwareMap.dcMotor.get( "lm");
+        leftFollow = hardwareMap.dcMotor.get( "lf");
 
-       rightMaster.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-       rightFollow.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-       leftMaster.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-       leftFollow.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightMaster.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFollow.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftMaster.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftFollow.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-       rightFollow.setDirection(DcMotorSimple.Direction.REVERSE);
-       rightMaster.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightFollow.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightMaster.setDirection(DcMotorSimple.Direction.REVERSE);
+    }
 
-   }
+    public void driveAll(double rightMasterSpeed, double rightFollowerSpeed, double leftMasterSpeed, double leftFollowerSpeed) {
+        rightMaster.setPower(rightMasterSpeed);
+        rightFollow.setPower(rightFollowerSpeed);
+        leftMaster.setPower(leftMasterSpeed);
+        leftFollow.setPower(leftFollowerSpeed);
 
-   public void driveAll(double rightMasterSpeed, double rightFollowerSpeed, double leftMasterSpeed, double leftFollowerSpeed) {
-       rightMaster.setPower(rightMasterSpeed);
-       rightFollow.setPower(rightFollowerSpeed);
-       leftMaster.setPower(leftMasterSpeed);
-       leftFollow.setPower(leftFollowerSpeed);
+        //telemetry.addData("rightMaster, rightFollow, leftMaster, leftFollow", "%.1 %.1 %.1 %.1", rightMasterSpeed, rightFollowerSpeed, leftMasterSpeed, leftFollowerSpeed);
+    }
 
-       //telemetry.addData("rightMaster, rightFollow, leftMaster, leftFollow", "%.1 %.1 %.1 %.1", rightMasterSpeed, rightFollowerSpeed, leftMasterSpeed, leftFollowerSpeed);
-   }
+    public void drive(double left, double right, boolean boost) {
+        if(boost){
+            driveAll(right, right, left, left);
+        } else {
+            driveAll(right*0.8, right*0.8, left*0.8, left*0.8);
+        }
 
-   public void drive(double left, double right, boolean boost) {
-       if(boost){
-           driveAll(right, right, left, left);
-       } else {
-           driveAll(right*0.8, right*0.8, left*0.8, left*0.8);
-       }
+    }
 
-   }
+    public void drive(double left, double right) {
+        drive(left, right, true);
+    }
 
-   public void driveArcade(double forwardPower, double turnPower, boolean boost){
-      double L = (2*forwardPower-turnPower* MOTOR.d)/2;
-      double R = turnPower*MOTOR.d+forwardPower;
+    //the amount of power to each motor when joystick is fully to one side
+    public static final double turnGoal = 0.4;
+    public static final double forwardGoal = 0.6;
+    public void driveArcade(double forwardPower, double turnPower){
+        double turnPart = turnPower*turnGoal;
+        double forwardPart = forwardPower*forwardGoal;
+        double L = forwardPart - turnPart;
+        double R = forwardPart + turnPart;
+        drive(L,R);
 
-      drive(L,R,boost);
-
-   }
-
+    }
     public void driveDistance(double distanceToDrive, double powerToDrive){
 
-       leftMaster.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-       rightMaster.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-       leftFollow.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-       rightFollow.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftMaster.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightMaster.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftFollow.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightFollow.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-       leftMaster.setTargetPosition((int)(distanceToDrive*ticksPerMeter));
-       rightMaster.setTargetPosition((int)(distanceToDrive*ticksPerMeter));
-       leftFollow.setTargetPosition((int)(distanceToDrive*ticksPerMeter));
-       rightFollow.setTargetPosition((int)(distanceToDrive*ticksPerMeter));
+        leftMaster.setTargetPosition((int)(distanceToDrive*ticksPerMeter));
+        rightMaster.setTargetPosition((int)(distanceToDrive*ticksPerMeter));
+        leftFollow.setTargetPosition((int)(distanceToDrive*ticksPerMeter));
+        rightFollow.setTargetPosition((int)(distanceToDrive*ticksPerMeter));
 
-       leftMaster.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-       leftFollow.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-       rightMaster.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-       rightFollow.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        leftMaster.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        leftFollow.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightMaster.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightFollow.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-       leftMaster.setPower(powerToDrive);
-       leftFollow.setPower(powerToDrive);
-       rightMaster.setPower(powerToDrive);
-       rightFollow.setPower(powerToDrive);
+        leftMaster.setPower(powerToDrive);
+        leftFollow.setPower(powerToDrive);
+        rightMaster.setPower(powerToDrive);
+        rightFollow.setPower(powerToDrive);
 
-       while(isBusy());
+        while(isBusy());
 
 
     }
 
     public void turnDistance(double angleToTurn, double powerToDrive){
-       double distancetoDrive = angleToTurn*MOTOR.d/2;
+        double distancetoDrive = angleToTurn*MOTOR.d/2;
 
-       leftMaster.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-       leftFollow.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-       rightMaster.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-       rightFollow.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftMaster.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftFollow.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightMaster.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightFollow.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-       leftMaster.setTargetPosition((int)(distancetoDrive*ticksPerMeter));
-       leftFollow.setTargetPosition((int)(distancetoDrive*ticksPerMeter));
-       rightMaster.setTargetPosition((int)(-distancetoDrive*ticksPerMeter));
-       rightFollow.setTargetPosition((int)(-distancetoDrive*ticksPerMeter));
+        leftMaster.setTargetPosition((int)(distancetoDrive*ticksPerMeter));
+        leftFollow.setTargetPosition((int)(distancetoDrive*ticksPerMeter));
+        rightMaster.setTargetPosition((int)(-distancetoDrive*ticksPerMeter));
+        rightFollow.setTargetPosition((int)(-distancetoDrive*ticksPerMeter));
 
         leftMaster.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         leftFollow.setMode(DcMotor.RunMode.RUN_TO_POSITION);
@@ -125,8 +134,7 @@ public class DriveBase {
 
     }
     public boolean isBusy(){
-       return leftFollow.isBusy() || leftMaster.isBusy() || rightFollow.isBusy() || rightMaster.getPowerFloat();
+        return leftFollow.isBusy() || leftMaster.isBusy() || rightFollow.isBusy() || rightMaster.getPowerFloat();
     }
 
 }
-
